9e44aded5d356066cc9b6ac7a5f1f066f3e874f5
Kawsar Emtiaz <kawsaremtiaz@Kawsars-MacBook-Pro.local>
2023-10-07 00:25:06 +0600

    Selected Color

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,7 @@ class HomeScreen extends StatefulWidget {
 
 class _HomeScreenState extends State<HomeScreen> {
   Color? _selectedColor;
+  Color selectedBtnColor = Colors.orange;
   String _selectedSize = "Medium";
 
   void _showSnackbar(String message) {
@@ -40,14 +41,17 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   Widget _buildButton(Color color, String size) {
-    return Padding(
-      padding: const EdgeInsets.all(8.0),
-      child: ElevatedButton(
-        onPressed: () => _onButtonPressed(color, size),
-        style: ElevatedButton.styleFrom(
-          backgroundColor: _selectedColor == color ? color.withOpacity(1) : color,
+    return SizedBox(
+      width: 100,
+      child: Padding(
+        padding: const EdgeInsets.all(5.0),
+        child: ElevatedButton(
+          onPressed: () => _onButtonPressed(color, size),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: _selectedColor == color ? selectedBtnColor : color,
+          ),
+          child: Text(size),
         ),
-        child: Text(size),
       ),
     );
   }
@@ -61,12 +65,12 @@ class _HomeScreenState extends State<HomeScreen> {
       body: Center(
         child: Wrap(
           children: <Widget>[
-            _buildButton(Colors.grey, 'S'),
-            _buildButton(Colors.grey, 'M'),
-            _buildButton(Colors.grey, 'L'),
+            _buildButton(Colors.red, 'S'),
+            _buildButton(Colors.green, 'M'),
+            _buildButton(Colors.blue, 'L'),
             _buildButton(Colors.grey, 'XL'),
-            _buildButton(Colors.grey, 'XXL'),
-            _buildButton(Colors.grey, 'XXXL'),
+            _buildButton(Colors.purple, 'XXL'),
+            _buildButton(Colors.teal, 'XXXL'),
           ],
         ),
       ),
